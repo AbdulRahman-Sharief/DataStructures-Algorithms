6fd98a8c14d705c37ade4901916dee49f3e4fbf0
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-08-16 18:28:25 +0300

    stack&Queues D.S.

---
+++ DataStructures/stacks&Queues.js
@@ -0,0 +1,69 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = this.first;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+
+        return ++this.size;
+    }
+
+    pop() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = this.first;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+
+        }
+        return ++this.size;
+    }
+    dequeue() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) this.last = null;
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+
+}
